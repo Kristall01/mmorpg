28f5818408ebf6e2a0b5fb978c5d009eaf466b47
Kristall01 <14259961+Kristall01@users.noreply.github.com>
2022-03-26 13:41:25 +0100

    applied material system instead of string. added material list if 0 arguments are given

--- backend/src/main/java/hu/kristall/rpg/command/commands/world/CommandWorldSpawnitem.java
+++ backend/src/main/java/hu/kristall/rpg/command/commands/world/CommandWorldSpawnitem.java
@@ -7,21 +7,45 @@ import hu.kristall.rpg.command.impl.SimpleCommand;
 import hu.kristall.rpg.command.senders.CommandSender;
 import hu.kristall.rpg.network.packet.out.PacketOutChat;
 import hu.kristall.rpg.world.Item;
+import hu.kristall.rpg.world.Material;
 
 public class CommandWorldSpawnitem extends SimpleCommand {
 	
 	public CommandWorldSpawnitem(CommandParent parent) {
-		super(parent, "spawnitem", "<item>", "spawns an item to your position");
+		super(parent, "spawnitem", "<item> [name]", "spawns an item to your position");
 	}
 	
 	@Override
 	protected void checkedExecute(CommandSender sender, String label, String[] args) {
-		if(!CommandCheckers.checkArgCount(sender, args, 1)) {
-			return;
-		}
 		CommandCheckers.checkWorldPlayerEntity(sender, entityHuman -> {
+			if(args.length == 0) {
+				Material[] mats = Material.values();
+				if(mats.length == 0) {
+					entityHuman.getWorldPlayer().getAsyncPlayer().connection.sendPacket(new PacketOutChat("Nincs lerakható item."));
+					return;
+				}
+				StringBuilder names = new StringBuilder();
+				for (Material value : Material.values()) {
+					names.append(", ");
+					names.append(value.name());
+				}
+				entityHuman.getWorldPlayer().getAsyncPlayer().connection.sendPacket(new PacketOutChat(names.substring(2)));
+				return;
+			}
 			Position pos = entityHuman.getPosition();
-			entityHuman.getWorld().spawnItem(new Item(args[0]), pos);
+			String name = null;
+			if(args.length > 1) {
+				name = args[1];
+			}
+			Material m;
+			try {
+				m = Material.valueOf(args[0]);
+			}
+			catch (IllegalArgumentException ex) {
+				entityHuman.getWorldPlayer().getAsyncPlayer().connection.sendPacket(new PacketOutChat("nincs ilyen item"));
+				return;
+			}
+			entityHuman.getWorld().spawnItem(new Item(m, name), pos);
 			entityHuman.getWorldPlayer().getAsyncPlayer().connection.sendPacket(new PacketOutChat("item spawned"));
 		});
 	}
