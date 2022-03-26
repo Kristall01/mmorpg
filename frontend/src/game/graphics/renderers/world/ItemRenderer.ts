c082fc0f00c20bce9b839fdc8d9c33878f6de827
Kristall01 <14259961+Kristall01@users.noreply.github.com>
2022-03-26 14:19:09 +0100

    deleted item renderer (merged to WorldRenderer)

--- frontend/src/game/graphics/renderers/world/ItemRenderer.ts
+++
@@ -1,9 +0,0 @@
-import { drawText, RenderContext } from "game/graphics/GraphicsUtils";
-import FloatingItem from "visual_model/FloatingItem";
-import WorldRenderer, { RenderConfig } from "./WorldRenderer";
-
-export const renderItem = (ctx: RenderContext, world: WorldRenderer, item: FloatingItem) => {
-	let pos = item.pos;
-	let canvasPos = world.translateXY(...pos);
-	drawText(ctx, canvasPos, item.item.name);
-}
