7b9ed830b57a0630dc38742a34b259dd6ee40eda
Kristall01 <14259961+Kristall01@users.noreply.github.com>
2022-05-29 21:07:14 +0200

    removed cli added windows library for tab completion

--- backend/src/main/java/Main.java
+++ backend/src/main/java/Main.java
@@ -12,76 +12,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.*;
-import java.net.URISyntaxException;
 
 public class Main {
 	
-	static String jarName() {
-		try {
-			return new File(Main.class
-				.getProtectionDomain()
-				.getCodeSource()
-				.getLocation()
-				.toURI()
-				.getPath()).getName();
-		}
-		catch (URISyntaxException e) {
-			return "server.jar";
-		}
-	}
-	
 	public static void main(String[] args) throws IOException {
-		/*List<List<String>> layers = List.of(List.of("GRASS","GRASS","GRASS"));
-		SavedLevel defaultLevel = new SavedLevel("defaultWorld", 10, 10, layers, Collections.emptyList(), Collections.emptyList());
-		
-		Map<String, SavedLevel> levels = new HashMap<>();
-		levels.put("defaultWorld", defaultLevel);
-		Map<String, SavedPlayer> players = new HashMap<>();
-		players.put("asd", new SavedPlayer("asd", "w0", new Position(0,0), 5, Collections.emptyList(), Collections.emptyList()));
-		Savefile savefile = new Savefile(levels, players, "defaultWorld");
-		
-		Path f = new File("/home/dominik/Asztal/savefile.json").toPath();
-		String serialized =  Utils.gson().toJson(savefile);
-		Files.write(f, serialized.getBytes(StandardCharsets.UTF_8));
-		try {
-			savefile = Utils.gson().fromJson(Files.readString(f), Savefile.class);
-		}
-		catch (Exception ex) {
-			ex.printStackTrace();
-		}
-		System.out.println("parsed savefile");
-		System.out.println(savefile);
-		System.exit(0);*/
-		
-		/*
-		Options opts = new Options();
-		opts.addOption(Option.builder()
-				.longOpt("port")
-				.required(false)
-				.hasArg(true)
-				.desc("tcp port to run server on. defaults to 8080")
-				.optionalArg(true)
-				.valueSeparator('=')
-				.numberOfArgs(1)
-				.argName("port")
-				.build());
-		
-		opts.addOption("h", "help", false, "print this help");
-		opts.addOption(Option.builder()
-				.longOpt("serve")
-				.required(false)
-				.hasArg(true)
-				.valueSeparator('=')
-				.desc("directory to serve static files from. skipping this argument will disable static file serving altogether")
-				.numberOfArgs(1)
-				.argName("path")
-				.build());
-		
-		if(args.length == 0) {
-			String jarname = jarName();
-			new HelpFormatter().printHelp("java -jar "+jarname+" [options]", opts);
-		}
-		*/
 		System.setProperty("org.slf4j.simpleLogger.showDateTime", "true");
 		System.setProperty("org.slf4j.simpleLogger.dateTimeFormat", /*"yyyy-MM-dd */"HH:mm:ss");
 		System.setProperty("org.slf4j.simpleLogger.showLogName", "true");
